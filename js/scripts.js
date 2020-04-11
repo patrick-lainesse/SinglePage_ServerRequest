6f5a99061c035cca74c0691e848cf4e44c0aab15
patrick-lainesse <patrick.lainesse@umontreal.ca>
2020-04-10 23:28:29 -0400

    Correction de plusieurs éléments du design et filtre blanc sur l'arrière-plan.

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,7 @@
 
 $(document).ready(function(){
     $('select').formSelect();
+    $(".dropdown-content>li>span").css("color", "#660066");
+    //$(".dropdown-content>li>span").css("background", "#006600");
+    //????
 });
